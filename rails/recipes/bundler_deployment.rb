d159ced25e8d5430510b1ba28c5276ebbe1434c1
Surendran Mahendran <surenspost@gmail.com>
2014-08-19 22:50:24 +0530

    Logging bundler command properly

--- rails/recipes/bundler_deployment.rb
+++ rails/recipes/bundler_deployment.rb
@@ -2,11 +2,12 @@ node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
   current_path = deploy[:current_path]
 
-  Chef::Log.info("Bundling with --deployment option")
+  Chef::Log.info("bundle install --deployment --without #{deploy[:ignore_bundler_groups].join(' ')} --path #{deploy[:home]}/.bundler/#{application}")
 
   execute 'bundle install --deployment' do
     cwd current_path
     user 'deploy'
+    
     command "bundle install --deployment --without #{deploy[:ignore_bundler_groups].join(' ')} --path #{deploy[:home]}/.bundler/#{application}"
     environment 'RAILS_ENV' => rails_env
   end
