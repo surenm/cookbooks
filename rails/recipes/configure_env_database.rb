5f1a2389f5a70b46a51c29c9936ad579812911da
Surendran Mahendran <surenspost@gmail.com>
2014-12-10 14:44:33 -0800

    Create database.yml from env

--- rails/recipes/configure_env_database.rb
+++ rails/recipes/configure_env_database.rb
@@ -1,7 +1,41 @@
+require 'cgi'
+require 'uri'
+
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
   current_path = deploy[:current_path]
+  database_url = node[:deploy][application][:environment_variables]['DATABASE_URL']
+  unless database_url.nil?
+    Chef::Log.info("Creating config/database.yml for #{rails_env} environment")
+    begin
+      uri = URI.parse(database_url)
+    rescue URI::InvalidURIError
+      raise "Invalid DATABASE_URL"
+    end
 
-  Chef::Log.info("Trying to set database for #{rails_env} environment")
-  Chef::Log.info("Environment variable for DATABASE_URL=#{node[:deploy][application][:environment_variables]['DATABASE_URL']}")
+    database = {
+      adapter: uri.scheme,
+      database: (uri.path || "").split("/")[1],
+      username: uri.user,
+      password: uri.password,
+      host: uri.host,
+      port: uri.port
+    }
+    database[:adapter] = "postgresql" if database[:adapter] == "postgres"
+
+    template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+      source "database_from_env.yml.erb"
+      cookbook 'rails'
+      mode "0660"
+      group deploy[:group]
+      owner deploy[:user]
+      variables(:database => database, :environment => deploy[:rails_env])
+
+      notifies :run, "execute[restart Rails app #{application}]"
+
+      only_if do
+        File.directory?("#{deploy[:deploy_to]}/shared/config/")
+      end
+    end
+  end
 end
