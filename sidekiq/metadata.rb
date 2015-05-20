f0c71f862f4c6e8096b1f17f87822be9438b2f1d
Surendran Mahendran <surendran.mahendran@gmail.com>
2015-05-20 19:10:02 +0530

    Remove rails and sidekiq recipe cookbooks

--- sidekiq/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'sidekiq'
-maintainer       'Polydice'
-maintainer_email 'no@body.com'
-license          'All rights reserved'
-description      'Runs sidekiq'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          '0.1.0'
