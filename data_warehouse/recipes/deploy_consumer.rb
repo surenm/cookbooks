90a0228092b40ab8de82a64884ee9397c62bce54
Surendran Mahendran <surendran.mahendran@gmail.com>
2015-05-21 16:04:32 +0530

    deploy_consumer task needs to be run as before_restart hook

--- data_warehouse/recipes/deploy_consumer.rb
+++ data_warehouse/recipes/deploy_consumer.rb
@@ -1,41 +1,43 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  env = deploy["environment"]
-  deploy_to = deploy["deploy_to"]
+  before_restart do
+    env = deploy["environment"]
+    deploy_to = deploy["deploy_to"]
 
-  Chef::Log.info("Pip install dependencies from requirements.txt...")
-  execute "/usr/bin/pip install -r #{deploy_to}/current/requirements.txt" do
-    user "root"
-    action :run
-  end
+    Chef::Log.info("Pip install dependencies from requirements.txt...")
+    execute "/usr/bin/pip install -r #{deploy_to}/current/requirements.txt" do
+      user "root"
+      action :run
+    end
 
-  command_generator_script = "#{deploy_to}/current/kinesis_consumer/kcl_command_generator.py"
-  consumer_properties_file = "#{deploy_to}/current/kinesis_consumer/consumer.properties"
-  java_binary = `which java`
+    command_generator_script = "#{deploy_to}/current/kinesis_consumer/kcl_command_generator.py"
+    consumer_properties_file = "#{deploy_to}/current/kinesis_consumer/consumer.properties"
+    java_binary = `which java`
 
-  Chef::Log.info("Generating consumer.properties for Kinesis Consumer...")
-  template consumer_properties_file do
-    source "consumer.properties.erb"
-    owner "root"
-    group "root"
-    mode 0755
-    variables(
-        :kinesis_stream_name => env["KINESIS_STREAM_NAME"],
-        :aws_region => env["AWS_REGION"],
-        :kinesis_consumer_script => "#{deploy_to}/current/kinesis_consumer/events_consumer.py",
-        :kinesis_consumer_app_name => "EventsConsumer"
-    )
-  end
+    Chef::Log.info("Generating consumer.properties for Kinesis Consumer...")
+    template consumer_properties_file do
+      source "consumer.properties.erb"
+      owner "root"
+      group "root"
+      mode 0755
+      variables(
+          :kinesis_stream_name => env["KINESIS_STREAM_NAME"],
+          :aws_region => env["AWS_REGION"],
+          :kinesis_consumer_script => "#{deploy_to}/current/kinesis_consumer/events_consumer.py",
+          :kinesis_consumer_app_name => "EventsConsumer"
+      )
+    end
 
-  Chef::Log.info("Adding consumer config for god...")
-  command_generator = "#{command_generator_script} --print_command --java #{java_binary} --properties #{consumer_properties_file}"
-  consumer_exe = `#{command_generator}`
+    Chef::Log.info("Adding consumer config for god...")
+    command_generator = "#{command_generator_script} --print_command --java #{java_binary} --properties #{consumer_properties_file}"
+    consumer_exe = `#{command_generator}`
 
-  god_monitor "kinesis_consumer" do
-    source "kinesis_consumer.god.erb"
-    consumer_exe consumer_exe
-    env env
-    action :nothing
+    god_monitor "kinesis_consumer" do
+      source "kinesis_consumer.god.erb"
+      consumer_exe consumer_exe
+      env env
+      action :nothing
+    end
   end
 end
