22d8e3d14d3ff504ad1f83e52a93ef689df47c70
Surendran Mahendran <surendran.mahendran@gmail.com>
2015-06-09 13:40:51 +0530

    Ruby blocks runs code during convergence and not during compilation

--- dw/recipes/deploy_consumers.rb
+++ dw/recipes/deploy_consumers.rb
@@ -3,16 +3,16 @@ include_recipe 'deploy'
 node[:deploy].each do |application, deploy|
     env = deploy["environment"]
     deploy_to = deploy["deploy_to"]
+    consumer_properties_file = "#{deploy_to}/current/kinesis_consumer/blast_stats_app_consumer.properties"
+    command_generator_script = "#{deploy_to}/current/kinesis_consumer/kcl_command_generator.py"
+    java_binary = `which java`.strip
+    command_generator = "#{command_generator_script} --print_command --java #{java_binary} --properties #{consumer_properties_file}"
 
     Chef::Log.info("Pip install dependencies from requirements.txt...")
     execute "/usr/local/bin/pip install -r #{deploy_to}/current/requirements.txt" do
       user "root"
       action :run
     end
-
-    command_generator_script = "#{deploy_to}/current/kinesis_consumer/kcl_command_generator.py"
-    consumer_properties_file = "#{deploy_to}/current/kinesis_consumer/blast_stats_app_consumer.properties"
-    java_binary = `which java`.strip
 
     Chef::Log.info("Generating consumer.properties for Kinesis Consumer...")
     template consumer_properties_file do
@@ -29,11 +29,17 @@ node[:deploy].each do |application, deploy|
     end
 
     Chef::Log.info("Adding consumer config for god...")
-    command_generator = "#{command_generator_script} --print_command --java #{java_binary} --properties #{consumer_properties_file}"
+
+    ruby_block "Calculate the consumer executable..." do
+      block do
+        consumer_exe = `#{command_generator}`
+      end
+      action :run
+    end
 
     god_monitor "kinesis_consumer" do
       source "kinesis_consumer.god.erb"
-      consumer_exe `#{command_generator}`
+      consumer_exe consumer_exe
       env env
       action :nothing
     end
